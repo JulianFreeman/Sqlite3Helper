57848dbdabec2e05a4730cdca993c6e3cfe18221
Julian Freeman <freemanjulian372@gmail.com>
2024-07-20 15:39:54 -0400

    v2.2.0

--- Sqlite3Helper.py
+++ Sqlite3Helper.py
@@ -1,6 +1,9 @@
 # coding: utf8
 from __future__ import annotations
+
+import os
 import sqlite3
+import time
 from dataclasses import dataclass
 from enum import StrEnum
 from os import PathLike
@@ -8,7 +11,7 @@ from os import PathLike
 from cryptography.fernet import Fernet, InvalidToken
 
 
-__version__ = "2.1.1"
+__version__ = "2.2.0"
 __version_info__ = tuple(map(int, __version__.split(".")))
 
 
@@ -35,7 +38,19 @@ class BlobType(object):
         return f"X'{self.data.hex()}'"
 
 
-def _encrypt_blob(blob: BlobType, fernet: Fernet) -> BlobType:
+class NotRandomFernet(Fernet):
+    """固定下来每次相同的 key 的加密结果相同，方便条件查询"""
+
+    def __init__(self, key: bytes | str, fix_time: int, fix_iv: bytes, backend=None):
+        super().__init__(key, backend)
+        self._fix_time = fix_time
+        self._fix_iv = fix_iv
+
+    def encrypt(self, data: bytes) -> bytes:
+        return self._encrypt_from_parts(data, self._fix_time, self._fix_iv)
+
+
+def _encrypt_blob(blob: BlobType, fernet: NotRandomFernet) -> BlobType:
     if fernet is None:
         raise ValueError("Key is not set")
     return BlobType(fernet.encrypt(blob.data))
@@ -131,21 +146,33 @@ class Expression(object):
 
 class Operand(object):
 
-    def __init__(self, column: Column | str):
+    def __init__(
+            self,
+            column: Column | str,
+            key: bytes = None,
+            fix_time: int = None,
+            fix_iv: bytes = None,
+    ):
         self._column = column
+        self._key = key
+        self._fix_time = fix_time
+        self._fix_iv = fix_iv
         self._name = column.name if isinstance(column, Column) else column
 
-    def equal_to(self, value):
+    def equal_to(self, value, not_: bool = False):
         if isinstance(self._column, Column):
             if self._column.data_type == DataType.BLOB and type(value) is str:
                 value = BlobType(value.encode("utf-8"))
-        return Expression(f"{self._name} = {_to_string(value)}")
-
-    def not_equal_to(self, value):
-        if isinstance(self._column, Column):
-            if self._column.data_type == DataType.BLOB and type(value) is str:
-                value = BlobType(value.encode("utf-8"))
-        return Expression(f"{self._name} =! {_to_string(value)}")
+            # 这里不能换成 elif
+            if self._key is not None and self._column.secure and isinstance(value, BlobType):
+                fix_time = self._fix_time if self._fix_time is not None else int(time.time())
+                fix_iv = self._fix_iv if self._fix_iv is not None else os.urandom(16)
+                try:
+                    value = _encrypt_blob(value, NotRandomFernet(self._key, fix_time, fix_iv))
+                except ValueError:
+                    pass
+        op = "!=" if not_ else "="
+        return Expression(f"{self._name} {op} {_to_string(value)}")
 
     def less_than(self, value):
         return Expression(f"{self._name} < {_to_string(value)}")
@@ -218,15 +245,23 @@ def order(column: Column | str | int,
 
 class Sqlite3Worker(object):
 
-    def __init__(self, db_name: str | PathLike[str] = ":memory:", key: bytes = None):
+    def __init__(
+            self,
+            db_name: str | PathLike[str] = ":memory:",
+            key: bytes = None,
+            fix_time: int = None,
+            fix_iv: bytes = None,
+    ):
         self._db_name = db_name
         self._conn = sqlite3.connect(db_name)
         self._cursor = self._conn.cursor()
         self._is_closed = False
         self._fernet = None
         if key is not None:
+            fix_time = fix_time if fix_time is not None else int(time.time())
+            fix_iv = fix_iv if fix_iv is not None else os.urandom(16)
             try:
-                self._fernet = Fernet(key)
+                self._fernet = NotRandomFernet(key, fix_time, fix_iv)
             except ValueError:
                 pass
 
@@ -415,18 +450,20 @@ class Sqlite3Worker(object):
             self._cursor.execute(statement)
             rows = self._cursor.fetchall()
             rows = [list(row) for row in rows]  # 将每行转成列表，方便替换解密数据
-            # 下面的整个循环都是为了找到需要解密的数据尝试解密，
-            # 因此一旦有一项解密失败，其他的都不用看了，直接原样返回
-            try:
-                for i in range(len(columns)):
-                    column = columns[i]
-                    if isinstance(column, Column) and column.secure:
-                        for row in rows:
-                            # 如果是加密的 BLOB 但是值不为 NULL 才加密
-                            if row[i] is not None and self._fernet is not None:
+            # 下面的整个循环都是为了找到需要解密的数据尝试解密
+            for i in range(len(columns)):
+                column = columns[i]
+                if isinstance(column, Column) and column.secure:
+                    for row in rows:
+                        # 如果是加密的 BLOB 但是值不为 NULL 才解密
+                        if row[i] is not None and self._fernet is not None:
+                            # 不管是key错误还是密文错误，都是 InvalidToken，貌似没法区分
+                            # 因此如果有的数据不是加密过的，应该跳过，不应该影响之后的密文解密，
+                            # 因此这里还是得继续循环下去
+                            try:
                                 row[i] = self._fernet.decrypt(row[i])
-            except InvalidToken:
-                pass
+                            except InvalidToken:
+                                pass
 
             return statement, rows
         else:
@@ -446,21 +483,26 @@ class Sqlite3Worker(object):
                 self._conn.commit()
         return statement
 
-    def update(self, table_name: str, new_values: list[tuple[Column, NullType | int | float | str | BlobType]],
+    def update(self, table_name: str, new_values: list[tuple[Column | str, NullType | int | float | str | BlobType]],
                where: Expression = None,
                *, execute: bool = True, commit: bool = True) -> str:
         new_values_str_ls = []
         for column, value in new_values:
-            # 支持将 NULL 值填入任意类型的列，除了 NOT NULL 限制的
-            if type(value) is NullType and column.nullable is True:
-                pass
-            elif _get_data_type(type(value)) != column.data_type:
-                raise ValueError(f"Type of {column.name} must be {column.data_type}, found {type(value)}")
+            if isinstance(column, Column):
+                # 支持将 NULL 值填入任意类型的列，除了 NOT NULL 限制的
+                if type(value) is NullType and column.nullable is True:
+                    pass
+                elif _get_data_type(type(value)) != column.data_type:
+                    raise ValueError(f"Type of {column.name} must be {column.data_type}, found {type(value)}")
 
-            if column.secure and type(value) is not NullType:
-                value = _encrypt_blob(value, self._fernet)
+                if column.secure and type(value) is not NullType:
+                    value = _encrypt_blob(value, self._fernet)
 
-            new_values_str_ls.append(f"{column.name} = {_to_string(value)}")
+                name = column.name
+            else:
+                name = column
+
+            new_values_str_ls.append(f"{name} = {_to_string(value)}")
 
         head = f"UPDATE {table_name}"
         body = f"{head} SET {', '.join(new_values_str_ls)}"
